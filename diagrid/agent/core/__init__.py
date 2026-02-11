50b1535642d079c1df238b8f9d486204a3ad8af9
Casper Nielsen <casper@diagrid.io>
2026-02-11 20:49:31 +0100

    feat: init of migration of metadata extraction pr from python-sdk
    Signed-off-by: Casper Nielsen <casper@diagrid.io>

---
+++ diagrid/agent/core/__init__.py
@@ -0,0 +1,29 @@
+from .metadata import (
+    AgentRegistryAdapter,
+    detect_framework,
+    find_agent_in_stack,
+)
+from .types import (
+    AgentMetadata,
+    AgentMetadataSchema,
+    LLMMetadata,
+    MemoryMetadata,
+    PubSubMetadata,
+    RegistryMetadata,
+    SupportedFrameworks,
+    ToolMetadata,
+)
+
+__all__ = [
+    'SupportedFrameworks',
+    'AgentMetadataSchema',
+    'AgentMetadata',
+    'LLMMetadata',
+    'PubSubMetadata',
+    'ToolMetadata',
+    'RegistryMetadata',
+    'MemoryMetadata',
+    'AgentRegistryAdapter',
+    'find_agent_in_stack',
+    'detect_framework',
+]
---
+++ diagrid/agent/core/types/__init__.py
@@ -0,0 +1,21 @@
+from .metadata import (
+    AgentMetadata,
+    AgentMetadataSchema,
+    LLMMetadata,
+    MemoryMetadata,
+    PubSubMetadata,
+    RegistryMetadata,
+    SupportedFrameworks,
+    ToolMetadata,
+)
+
+__all__ = [
+    'AgentMetadata',
+    'AgentMetadataSchema',
+    'LLMMetadata',
+    'MemoryMetadata',
+    'PubSubMetadata',
+    'RegistryMetadata',
+    'SupportedFrameworks',
+    'ToolMetadata',
+]
